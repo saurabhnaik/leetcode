1b04a68a076f73f3957befac57b1129d57fb7356
Saurabh Naik <saurabmi2@gmail.com>
2022-05-18 23:11:58 +0530

    Update TwoSum.java
    updated with o(n) solution

--- TwoSum.java
+++ TwoSum.java
@@ -2,7 +2,7 @@ class TwoSum {
     public int[] twoSum(int[] nums, int target) {
          
         //n square solution
-    
+    /*
         for(int i=0;i<nums.length;i++)
             {
                 for(int j=0;j<nums.length;j++)
@@ -13,6 +13,16 @@ class TwoSum {
                         return new int[]{i,j};
                 }
             }
+        return new int[]{};*/
+        
+        Map<Integer,Integer> map=new HashMap<Integer,Integer>();
+        
+            for(int i=0;i<nums.length;i++)
+            {
+                if(map.containsKey(target-nums[i]))
+                    return new int[]{map.get(target-nums[i]),i};
+                map.put(nums[i],i);
+                
+            }
         return new int[]{};
-
 }}
