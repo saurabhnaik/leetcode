c198bd48dcf5d40bdc35fc4833219d6421fd2413
Saurabh Naik <saurabmi2@gmail.com>
2022-05-18 23:02:59 +0530

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,18 @@
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+         
+        //n square solution
+    
+        for(int i=0;i<nums.length;i++)
+            {
+                for(int j=0;j<nums.length;j++)
+                {
+                    if(i==j)
+                        continue;
+                    if(nums[i]+nums[j]==target)
+                        return new int[]{i,j};
+                }
+            }
+        return new int[]{};
+
+}}
